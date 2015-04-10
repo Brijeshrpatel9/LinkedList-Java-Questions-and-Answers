5c3768a7313ed7f9263491ed365febd40068d804
Brijesh Patel <Brijeshrpatel9@users.noreply.github.com>
2015-04-10 13:19:00 -0500

    Create LinkedListKthElementFromEnd.java

---
+++ LinkedListKthElementFromEnd.java
@@ -0,0 +1,43 @@
+
+public class LinkedListKthElementFromEnd {
+	
+	public static void main(String argv[])
+	{
+		Node head = new Node(1);
+		head.append(2).append(3).append(4).append(5).append(6).append(7).append(8).append(9);
+		Node result = findKthElement(head, 4);
+		System.out.println(result.value);
+	}
+	
+	public static Node findKthElement(Node head, int k)
+	{
+		System.out.println(k + "th element from end ");
+		Node ahead = head;
+		while(k-- > 0)
+			ahead = ahead.next;
+		while(ahead != null)
+		{
+			head = head.next;
+			ahead = ahead.next;
+		}
+		return head;
+	}
+
+	static class Node
+	{
+		public Node next;
+		public int value;
+		
+		public Node(int value)
+		{
+			this.value = value;
+		}
+		
+		public Node append(int value)
+		{
+			Node n = new Node(value);
+			next = n;
+			return n;
+		}
+	}
+}
